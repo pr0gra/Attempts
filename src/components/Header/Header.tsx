8106df78307471342d3153f05084702b2a9f73be
pr0gra <denisovProg@gmail.com>
2022-11-03 01:33:26 +0500

    comment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import gitIcon from "../../assets/github-icon.svg";
 
 export function Header() {
   return (
     <header className={styles["header"]}>
       <Link to={"/"} className={styles["header-link"]}>
-        <img
-          className={styles["github-icon"]}
-          src="/src/assets/github-icon.svg"
-          alt="Icon"
-        />
+        <img className={styles["github-icon"]} src={gitIcon} alt="Icon" />
         <h2 className={styles["header-title"]}>Finding GitHub Issues</h2>
       </Link>
     </header>
